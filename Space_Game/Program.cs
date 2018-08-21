9533bbcd15c5a7783198e1718f48f0c1727837ee
Alexander Gough <gougha3@my.erau.edu>
2018-08-21 09:30:18 -0400

    If applied this commit will: Allow travel

--- Space_Game/Program.cs
+++ Space_Game/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Space_Game
 {
-   
+
     class Program
     {
         static void Main()
@@ -20,50 +20,190 @@ namespace Space_Game
             Console.WriteLine("Each Element has a different value between the different planets.");
             Console.WriteLine("travel to them to find out and make a profit!");
             Console.WriteLine("Press enter to continue");
-        
+            PlanetScreenEarth();
             Console.ReadLine();
+        }
 
 
 
-            
-                
 
-        
-           /*
+        static void PlanetScreenEarth()
+        {
             string input = "";
-            Console.WriteLine("Press a to Trade");
-            Console.WriteLine("Press b to Upgrade/Refuel");
-            Console.WriteLine("Press c to Travel");
+            Console.WriteLine("Welcome to Earth");
+            Console.WriteLine("To Enter the Trading post Press: a");
+            Console.WriteLine("To enter the Upgrade shop Press: b");
+            Console.WriteLine("To Travel Press: c");
+
             input = Console.ReadLine();
             if (input == "a")
             {
-                input = "";
-                Console.WriteLine("Press a to Buy");
-                Console.WriteLine("Press b to sell");
-                Console.WriteLine("Press c to Return to menu");
-                Console.ReadLine();
-
-                if (input == "a") 
-                {
-                    Console.WriteLine("Press a for: Oxygen 15 credits");
-                    Console.WriteLine("Press b for: Nitrogen 5 credits");
-                    Console.WriteLine("press c for: Co2 10 credits");
-                    Console.ReadLine();
-                }
+                TradingPostEarth();
             }
-            if  (input == "b")
+            if (input == "b")
             {
-                Console.WriteLine("Press a to Upgrade Warp Drive");
-                Console.WriteLine("Press b to refuel");
-                Console.WriteLine("Press c to Return to menu");
-                Console.ReadLine();
+                UpgradeShop();
             }
-
-    */
-         
-          
+            if (input == "c")
+            {
+                TravelPlanetsEarth();
+            }
 
 
         }
+        static void TradingPostEarth()
+        {
+            string input = "";
+            Console.WriteLine("To buy Oxygen (15 Credits) press: a");
+            Console.WriteLine("To buy Nitrogen (5) press: b");
+            Console.WriteLine("To buy Co2 (10) press: c");
+            input = Console.ReadLine();
+
+        }
+
+        static void UpgradeShop()
+        {
+            string input = "";
+            Console.WriteLine("To upgrade Warp Drive Press: a");
+            Console.WriteLine("To upgrade Cargo Hold Press: b");
+            input = Console.ReadLine();
+        }
+        static void TravelPlanetsEarth()
+        {
+            string input = "";
+            Console.WriteLine("To travel to Alpha Centauri Press: a");
+            Console.WriteLine("To travel to m63 Press: b");
+            Console.WriteLine("To return to main menu Press: c");
+            input = Console.ReadLine();
+            if (input == "a")
+            {
+                PlanetScreenAlphaCentauri();
+            }
+            if (input == "b")
+            {
+                PlanetScreenM63();
+            }
+            else
+            {
+                PlanetScreenEarth();
+            }
+        }
+
+
+
+
+        static void PlanetScreenAlphaCentauri()
+        {
+            string input = "";
+            Console.WriteLine("Welcome to Alpha Centauri");
+            Console.WriteLine("To Enter the Trading post Press: a");
+            Console.WriteLine("To enter the Upgrade shop Press: b");
+            Console.WriteLine("To Travel Press: c");
+            input = Console.ReadLine();
+            if (input == "a")
+            {
+                TradingPostAlphaCentauri();
+            }
+            if (input == "b")
+            {
+                UpgradeShop();
+            }
+            if (input == "c")
+            {
+                TravelPlanetsAlphaCentauri();
+            }
+            else
+            {
+                PlanetScreenAlphaCentauri();
+            }
+
+        }
+        static void TradingPostAlphaCentauri()
+        {
+            string input = "";
+            Console.WriteLine("To buy Oxygen (20 Credits) press: a");
+            Console.WriteLine("To buy Nitrogen (10) press: b");
+            Console.WriteLine("To buy Co2 (5) press: c");
+            input = Console.ReadLine();
+
+        }
+        static void TravelPlanetsAlphaCentauri()
+        {
+            string input = "";
+            Console.WriteLine("To travel to Earth Press: a");
+            Console.WriteLine("To travel to m63 Press: b");
+            Console.WriteLine("To return to main menu Press: c");
+            input = Console.ReadLine();
+            if (input == "a")
+            {
+                PlanetScreenEarth();
+            }
+            if (input == "b")
+            {
+                PlanetScreenM63();
+            }
+            else
+            {
+                PlanetScreenAlphaCentauri();
+            }
+        }
+
+        static void PlanetScreenM63()
+        {
+            string input = "";
+            Console.WriteLine("Welcome to M63");
+            Console.WriteLine("To Enter the Trading post Press: a");
+            Console.WriteLine("To enter the Upgrade shop Press: b");
+            Console.WriteLine("To Travel Press: c");
+            input = Console.ReadLine();
+            if (input == "a")
+            {
+                TradingPostM63();
+            }
+            if (input == "b")
+            {
+                UpgradeShop();
+            }
+            if (input == "c")
+            {
+                TravelPlanetsM63();
+            }
+            else
+            {
+                PlanetScreenM63();
+            }
+
+        }
+        static void TradingPostM63()
+        {
+            string input = "";
+            Console.WriteLine("To buy Oxygen (10 Credits) press: a");
+            Console.WriteLine("To buy Nitrogen (25 Credits) press: b");
+            Console.WriteLine("To buy Co2 (10 Credits) press: c");
+            Console.WriteLine("To return to menu Press: d");
+            input = Console.ReadLine();
+
+        }
+        static void TravelPlanetsM63()
+        {
+            string input = "";
+            Console.WriteLine("To travel to Earth Press: a");
+            Console.WriteLine("To travel to Alpha Centauri Press: b");
+            Console.WriteLine("To return to main menu Press: c");
+            input = Console.ReadLine();
+            if (input == "a")
+            {
+                PlanetScreenEarth();
+            }
+            if (input == "b")
+            {
+                PlanetScreenAlphaCentauri();
+            }
+            else
+            {
+                PlanetScreenM63();
+            }
+        }
     }
 }
+
