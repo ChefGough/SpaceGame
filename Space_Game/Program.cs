ec98c83d187fe00a3edfe69cacdd2ee4da479490
Alexander Gough <gougha3@my.erau.edu>
2018-08-21 08:16:05 -0400

    Added Start menu

--- Space_Game/Program.cs
+++ Space_Game/Program.cs
@@ -2,11 +2,68 @@
 
 namespace Space_Game
 {
+   
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to Space Game Main Menu:");
+            Console.WriteLine("Please enter your character's name");
+            string CharacterName = Console.ReadLine();
+            Console.WriteLine("Your chosen name is: " + CharacterName);
+            Console.WriteLine("Hello " + CharacterName + " The year is 2225. You have just bought your first starship.");
+            Console.WriteLine("Your cargo hold can only hold 1 unit of cargo. You have only 25 credits left");
+            Console.WriteLine("Oxygen in available on earth for: 15 credits");
+            Console.WriteLine("Nitrogen is available for 5 credits");
+            Console.WriteLine("Co2 is available for 10 credits.");
+            Console.WriteLine("Each Element has a different value between the different planets.");
+            Console.WriteLine("travel to them to find out and make a profit!");
+            Console.WriteLine("Press enter to continue");
+        
+            Console.ReadLine();
+
+
+
+            
+                
+
+        
+           /*
+            string input = "";
+            Console.WriteLine("Press a to Trade");
+            Console.WriteLine("Press b to Upgrade/Refuel");
+            Console.WriteLine("Press c to Travel");
+            input = Console.ReadLine();
+            if (input == "a")
+            {
+                input = "";
+                Console.WriteLine("Press a to Buy");
+                Console.WriteLine("Press b to sell");
+                Console.WriteLine("Press c to Return to menu");
+                Console.ReadLine();
+
+                if (input == "a") 
+                {
+                    Console.WriteLine("Press a for: Oxygen 15 credits");
+                    Console.WriteLine("Press b for: Nitrogen 5 credits");
+                    Console.WriteLine("press c for: Co2 10 credits");
+                    Console.ReadLine();
+                }
+            }
+            if  (input == "b")
+            {
+                Console.WriteLine("Press a to Upgrade Warp Drive");
+                Console.WriteLine("Press b to refuel");
+                Console.WriteLine("Press c to Return to menu");
+                Console.ReadLine();
+            }
+
+    */
+         
+          
+
+
         }
     }
 }
